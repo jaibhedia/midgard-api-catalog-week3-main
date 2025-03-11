e2a944bc9eb38044314991c2db8146bbae56df8b
jaibhedia <jaibhedia@gmail.com>
2025-03-11 10:23:20 +0530

    reduced the fetching time

--- api/src/scheduler.rs
+++ api/src/scheduler.rs
@@ -4,7 +4,7 @@ use std::sync::Arc;
 use tokio::time::{self, Duration};
 
 pub async fn start_scheduler(db_pool: Arc<PgPool>) {
-    let mut interval = time::interval(Duration::from_secs(3600));
+    let mut interval = time::interval(Duration::from_secs(120));
     loop {
         interval.tick().await;
         populate_db(&db_pool).await;
